46de1cb799e34a89a114814a36e837fd3a58ea7c
Fr4bz <francesco.baldarotta@gmail.com>
2021-09-10 17:52:26 +0200

    a

--- prova/js/script.js
+++ prova/js/script.js
@@ -8,7 +8,7 @@
         if (!navbarCollapsible) {
             return;
         }
-        if (document.body.scrollTop < 150 || document.documentElement.scrollTop < 150) {
+        if (document.body.scrollTop < 80 || document.documentElement.scrollTop < 80) {
             navbarCollapsible.classList.remove('navbar-shrink')
             
         } else {
