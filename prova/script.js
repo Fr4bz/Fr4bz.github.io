79028bb1fe5f131db2ccdf7864e41ca67f484795
Fr4bz <francesco.baldarotta@gmail.com>
2021-09-10 17:26:33 +0200

    a

--- prova/script.js
+++ prova/script.js
@@ -2,15 +2,24 @@ window.onscroll = function() {scrollFunction()};
 
 
     // Navbar shrink function
-    var navbarShrink = function () {
+    function scrollFunction() {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
             return;
         }
-        if (window.scrollY === 000) {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
             navbarCollapsible.classList.remove('navbar-shrink')
+            
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
         }
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+              document.getElementById("navbar").style.padding = "30px 10px";
+            } else {
+              document.getElementById("navbar").style.padding = "80px 10px";
+            }
+          }
         
     };
